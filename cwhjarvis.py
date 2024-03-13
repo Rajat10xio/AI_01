7e6e882259192d5a08ba3694848fa06dcbd99da1
Praphull Rahangdale <162778782+praphullrahangdale@users.noreply.github.com>
2024-03-13 11:57:50 +0530

    Update cwhjarvis.py

--- cwhjarvis.py
+++ cwhjarvis.py
@@ -1,46 +1,103 @@
 import pyttsx3
-import datetime
-import pyaudio
 import speech_recognition as sr
+import datetime
 import wikipedia
+import webbrowser
+import os
 
 engine = pyttsx3.init('sapi5')
-voices = engine.getProperty("voices") 
-#print(voices[0].id)
-engine.setProperty('voice',voices[0].id)
+voices = engine.getProperty('voices')
+print(voices[1].id)
+engine.setProperty('voices', voices[1].id)
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-def wishme():
-    hour =  int(datetime.datetime.now().hour)
-    if 1 <= hour < 12:
-        speak("Good Morning!")
-    elif 12 <= hour <  18:
-        speak("Good Afternoon!")
-    else: 
-        speak("Good Evening!")
-    speak("i am rojaa.  please tell me how can i help you")   
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12 :
+        speak("Good morning!")
 
-def takecommand():
-    r = sr.Recognizer()
+    elif hour>=12 and hour<18 :
+        speak("Good afternoon!")
+
+    else:
+        speak("Good evening!") 
+
+    speak("Hello, I am jarvis sir. how may I help you")
+
+def takeCommand():
+    #It takes microphone input from user and return string output    
+    recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        print("listening.....")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-        try:
-            query = r.recognize_bing(audio,language="en-in")
-            print(f"user said :",{query})
-            return query
-        except Exception as e:
-            return "some  error occured"  
+        print("Listening....")
+        recognizer.pause_threshold = 1
+        audio = recognizer.listen(source)
+      
+    try:
+        print("Recognizing...") 
+        query = recognizer.recognize_google(audio, language='en-in')  
+        print("User said :",query)
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+
+# def sendEmail(to, content):
 
 
-if __name__=="__main__" :
-    wishme() 
-    while True :
-        query = takecommand()
-        speak(query)
-#i love you 
-# chal hat
+if __name__ == "__main__":
+    wishMe() 
+    while True:
+        query = takeCommand().lower()
+        
+        # logic for excuting tasks based on query
+        if 'wikipedia' in query:
+            speak("Searching wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia...")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com") 
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")  
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\PRAPHULL RAHANGDALE\\Music\\Indian Walk - Nico Staf.mp3'
+            # songs = os.listdir(music_dir)
+            # print(songs)
+            os.startfile(os.path.join(music_dir)) 
+
+        elif 'stop' in query:
+            break    
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            path = 'C:\\Program Files\\Microsoft VS Code\\Code.exe'
+            os.startfile(path)  
+
+        elif 'send email to praphull' in query:
+            try:
+                speak("What should I say")    
+                content = takeCommand()
+                to = "praphullrahangdale.gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent...")
+
+            except Exception as e:
+                print("e")
+                speak("Sorry sir, I am not able to send this email..")      
+
+        
